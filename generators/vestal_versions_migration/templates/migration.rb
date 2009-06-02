731d3d987f4b4f23455904fe6b4c2fc2fd9c0cab
laserlemon <steve@laserlemon.com>
2009-06-02 19:15:34 -0400

    Added a migration generator.

---
+++ generators/vestal_versions_migration/templates/migration.rb
@@ -0,0 +1,20 @@
+class CreateVestalVersions < ActiveRecord::Migration
+  def self.up
+    create_table :versions do |t|
+      t.belongs_to :versioned, :polymorphic => true
+      t.text :changes
+      t.integer :number
+      t.datetime :created_at
+    end
+    
+    change_table :versions do |t|
+      t.index [:versioned_type, :versioned_id]
+      t.index :number
+      t.index :created_at
+    end
+  end
+
+  def self.down
+    drop_table :versions
+  end
+end
