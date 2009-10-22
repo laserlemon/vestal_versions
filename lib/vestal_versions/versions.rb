c9edd41710aff78615e75983344123929df40a53
laserlemon <steve.richert@gmail.com>
2009-10-22 02:41:33 -0400

    Just cleaned up the versions association methods.

--- lib/vestal_versions/versions.rb
+++ lib/vestal_versions/versions.rb
@@ -3,6 +3,7 @@ module VestalVersions
     def between(from, to)
       from_number, to_number = number_at(from), number_at(to)
       return [] if from_number.nil? || to_number.nil?
+
       condition = (from_number == to_number) ? to_number : Range.new(*[from_number, to_number].sort)
       all(
         :conditions => {:number => condition},
@@ -12,20 +13,20 @@ module VestalVersions
 
     def at(value)
       case value
-        when Version then value
-        when Numeric then find_by_number(value.floor)
         when Date, Time then last(:conditions => ["#{aliased_table_name}.created_at <= ?", value.to_time])
-        when Symbol then respond_to?(value) ? send(value) : nil
+        when Numeric then find_by_number(value.floor)
         when String then first(:conditions => {:tag => value})
+        when Symbol then respond_to?(value) ? send(value) : nil
+        when Version then value
       end
     end
 
     def number_at(value)
       case value
-        when Version then value.number
-        when Numeric then value.floor
         when Date, Time then at(value).try(:number) || 1
-        when Symbol, String then at(value).try(:number)
+        when Numeric then value.floor
+        when String, Symbol then at(value).try(:number)
+        when Version then value.number
       end
     end
   end
