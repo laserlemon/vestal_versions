402dc14928d8c98156b32431b1a8ab58a277251d
laserlemon <steve.richert@gmail.com>
2009-10-21 23:30:50 -0400

    Moved change-processing methods to the Changes module.

--- lib/vestal_versions.rb
+++ lib/vestal_versions.rb
@@ -19,6 +19,7 @@ module VestalVersions
       after_update :create_version, :if => :needs_version?
 
       include InstanceMethods
+      include Changes
       include Reload
     end
   end
@@ -58,25 +59,6 @@ module VestalVersions
 
       def reverted?
         version != last_version
-      end
-
-      def changes_between(from, to)
-        from_number, to_number = versions.number_at(from), versions.number_at(to)
-        return {} if from_number == to_number
-        chain = versions.between(from_number, to_number).reject(&:initial?)
-        return {} if chain.empty?
-
-        backward = from_number > to_number
-        backward ? chain.pop : chain.shift unless [from_number, to_number].include?(1)
-
-        chain.inject({}) do |changes, version|
-          version.changes.each do |attribute, change|
-            change.reverse! if backward
-            new_change = [changes.fetch(attribute, change).first, change.last]
-            changes.update(attribute => new_change)
-          end
-          changes
-        end
       end
 
       def revert_to(value)
--- lib/vestal_versions/changes.rb
+++ lib/vestal_versions/changes.rb
@@ -1,4 +1,22 @@
 module VestalVersions
   module Changes
+    def changes_between(from, to)
+      from_number, to_number = versions.number_at(from), versions.number_at(to)
+      return {} if from_number == to_number
+      chain = versions.between(from_number, to_number).reject(&:initial?)
+      return {} if chain.empty?
+
+      backward = from_number > to_number
+      backward ? chain.pop : chain.shift unless [from_number, to_number].include?(1)
+
+      chain.inject({}) do |changes, version|
+        version.changes.each do |attribute, change|
+          change.reverse! if backward
+          new_change = [changes.fetch(attribute, change).first, change.last]
+          changes.update(attribute => new_change)
+        end
+        changes
+      end
+    end
   end
 end
