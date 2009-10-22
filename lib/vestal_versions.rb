e8a8357fef8b34a4278d815d7ea6a532afafc755
laserlemon <steve.richert@gmail.com>
2009-10-22 00:17:53 -0400

    Refactored the base inclusion (now extension) of VestalVersions.

--- lib/vestal_versions.rb
+++ lib/vestal_versions.rb
@@ -3,29 +3,19 @@
 end
 
 module VestalVersions
-  def self.included(base)
-    base.extend ClassMethods
-  end
+  def versioned(options = {})
+    class_inheritable_accessor :version_only_columns
+    self.version_only_columns = Array(options[:only]).map(&:to_s).uniq if options[:only]
+    class_inheritable_accessor :version_except_columns
+    self.version_except_columns = Array(options[:except]).map(&:to_s).uniq if options[:except]
 
-  module ClassMethods
-    def versioned(options = {})
-      class_inheritable_accessor :version_only_columns
-      self.version_only_columns = Array(options[:only]).map(&:to_s).uniq if options[:only]
-      class_inheritable_accessor :version_except_columns
-      self.version_except_columns = Array(options[:except]).map(&:to_s).uniq if options[:except]
+    has_many :versions, :class_name => '::VestalVersions::Version', :as => :versioned, :order => 'versions.number ASC', :dependent => :delete_all, :extend => Versions
 
-      has_many :versions, :as => :versioned, :order => 'versions.number ASC', :dependent => :delete_all, :extend => Versions
-
-      include InstanceMethods
-      include Changes
-      include Creation
-      include Reversion
-      include Reload
-    end
-  end
-
-  module InstanceMethods
+    include Changes
+    include Creation
+    include Reversion
+    include Reload
   end
 end
 
-ActiveRecord::Base.send(:include, VestalVersions)
+ActiveRecord::Base.extend(VestalVersions)
