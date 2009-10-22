8cb7e453198f6a35ff890897858c6ee0ba55615a
laserlemon <steve.richert@gmail.com>
2009-10-22 01:43:27 -0400

    Renamed the needs_version? method to create_version? for consistency with the control features (that I'm about to add).

--- lib/vestal_versions/creation.rb
+++ lib/vestal_versions/creation.rb
@@ -2,12 +2,12 @@ module VestalVersions
   module Creation
     def self.included(base)
       base.class_eval do
-        after_update :create_version, :if => :needs_version?
+        after_update :create_version, :if => :create_version?
       end
     end
 
     private
-      def needs_version?
+      def create_version?
         !(versioned_columns & changed).empty?
       end
 
