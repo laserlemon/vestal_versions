917948cc2fadc4d1de7d0d3e1d186513e93a0579
laserlemon <steve.richert@gmail.com>
2009-10-21 23:53:14 -0400

    Refactored the changes_between method to use some handy new Hash extension methods.

--- lib/vestal_versions/changes.rb
+++ lib/vestal_versions/changes.rb
@@ -1,5 +1,9 @@
 module VestalVersions
   module Changes
+    def self.included(base)
+      Hash.send(:include, HashMethods)
+    end
+
     def changes_between(from, to)
       from_number, to_number = versions.number_at(from), versions.number_at(to)
       return {} if from_number == to_number
@@ -10,12 +14,36 @@ module VestalVersions
       backward ? chain.pop : chain.shift unless [from_number, to_number].include?(1)
 
       chain.inject({}) do |changes, version|
-        version.changes.each do |attribute, change|
-          change.reverse! if backward
-          new_change = [changes.fetch(attribute, change).first, change.last]
-          changes.update(attribute => new_change)
+        changes.append_changes!(backward ? version.changes.reverse_changes : version.changes)
+      end
+    end
+
+    module HashMethods
+      def append_changes(changes)
+        changes.inject(self) do |new_changes, (attribute, change)|
+          new_change = [new_changes.fetch(attribute, change).first, change.last]
+          new_changes.merge!(attribute => new_change)
         end
-        changes
+      end
+
+      def append_changes!(changes)
+        replace(append_changes(changes))
+      end
+
+      def prepend_changes(changes)
+        changes.append_changes(self)
+      end
+
+      def prepend_changes!(changes)
+        replace(prepend_changes(changes))
+      end
+
+      def reverse_changes
+        inject({}){|nc,(a,c)| nc.merge!(a => c.reverse) }
+      end
+
+      def reverse_changes!
+        replace(reverse(perform))
       end
     end
   end
