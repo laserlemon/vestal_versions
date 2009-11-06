410f00ad372eee8e6700d6732a34a2ff663fddb4
laserlemon <steve.richert@gmail.com>
2009-11-06 09:55:51 -0500

    Built the initial user storage functionality in its own feature module.

---
+++ lib/vestal_versions/users.rb
@@ -0,0 +1,39 @@
+module VestalVersions
+  module Users
+    def self.included(base)
+      base.class_eval do
+        attr_accessor :updated_by
+        alias_method_chain :version_attributes, :user
+
+        Version.send(:include, VersionMethods)
+      end
+    end
+
+    private
+      def version_attributes_with_user
+        version_attributes_without_user.merge(:user => updated_by)
+      end
+
+    module VersionMethods
+      def self.included(base)
+        base.class_eval do
+          belongs_to :user, :polymorphic => true
+
+          alias_method_chain :user, :name
+          alias_method_chain :user=, :name
+        end
+      end
+
+      def user_with_name
+        user_without_name || user_name
+      end
+
+      def user_with_name=(value)
+        case value
+          when ActiveRecord::Base then self.user_without_name = value
+          else self.user_name = value
+        end
+      end
+    end
+  end
+end
