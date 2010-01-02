50042ec5d01d16536baadd82e759f22cf09a3586
laserlemon <steve.richert@gmail.com>
2010-01-02 16:23:56 -0500

    Added more tests to the "versions" association methods, namely the all-important "at" and "number_at" methods.

--- test/versions_test.rb
+++ test/versions_test.rb
@@ -8,10 +8,10 @@ class VersionsTest < Test::Unit::TestCase
       time = names.size.hours.ago
       names.each do |name|
         @user.update_attribute(:name, name)
-        time += 1.hour
-        @user.versions.last.try(:update_attribute, :created_at, time)
-        @times[@user.version] = time
         @user.tag_version(@user.version.to_s)
+        time += 1.hour
+        @user.versions.last.update_attribute(:created_at, time)
+        @times[@user.version] = time
       end
     end
 
@@ -63,6 +63,110 @@ class VersionsTest < Test::Unit::TestCase
 
     should 'return an empty array when searching after an invalid version value' do
       assert_equal [], @user.versions.after(nil)
+    end
+
+    should 'be fetchable by version number' do
+      @times.keys.each do |number|
+        assert_kind_of VestalVersions::Version, @user.versions.at(number)
+        assert_equal number, @user.versions.at(number).number
+      end
+    end
+
+    should 'be fetchable by tag' do
+      @times.keys.map{|n| [n, n.to_s] }.each do |number, tag|
+        assert_kind_of VestalVersions::Version, @user.versions.at(tag)
+        assert_equal number, @user.versions.at(tag).number
+      end
+    end
+
+    should "be fetchable by the exact time of a version's creation" do
+      @times.each do |number, time|
+        assert_kind_of VestalVersions::Version, @user.versions.at(time)
+        assert_equal number, @user.versions.at(time).number
+      end
+    end
+
+    should "be fetchable by any time after the model's creation" do
+      @times.each do |number, time|
+        assert_kind_of VestalVersions::Version, @user.versions.at(time + 30.minutes)
+        assert_equal number, @user.versions.at(time + 30.minutes).number
+      end
+    end
+
+    should "return nil when fetching a time before the model's creation" do
+      creation = @times.values.min
+      assert_nil @user.versions.at(creation - 1.second)
+    end
+
+    should 'be fetchable by an association extension method' do
+      assert_kind_of VestalVersions::Version, @user.versions.at(:first)
+      assert_kind_of VestalVersions::Version, @user.versions.at(:last)
+      assert_equal @times.keys.min, @user.versions.at(:first).number
+      assert_equal @times.keys.max, @user.versions.at(:last).number
+    end
+
+    should 'be fetchable by a version object' do
+      @times.keys.each do |number|
+        version = @user.versions.at(number)
+        assert_kind_of VestalVersions::Version, version
+        assert_kind_of VestalVersions::Version, @user.versions.at(version)
+        assert_equal number, @user.versions.at(version).number
+      end
+    end
+
+    should 'return nil when fetching an invalid version value' do
+      assert_nil @user.versions.at(nil)
+    end
+
+    should 'provide a version number for any given numeric version value' do
+      @times.keys.each do |number|
+        assert_kind_of Fixnum, @user.versions.number_at(number)
+        assert_kind_of Fixnum, @user.versions.number_at(number + 0.5)
+        assert_equal @user.versions.number_at(number), @user.versions.number_at(number + 0.5)
+      end
+    end
+
+    should 'provide a version number for a valid tag' do
+      @times.keys.map{|n| [n, n.to_s] }.each do |number, tag|
+        assert_kind_of Fixnum, @user.versions.number_at(tag)
+        assert_equal number, @user.versions.number_at(tag)
+      end
+    end
+
+    should 'return nil when providing a version number for an invalid tag' do
+      assert_nil @user.versions.number_at('INVALID')
+    end
+
+    should 'provide a version number of a version corresponding to an association extension method' do
+      assert_kind_of VestalVersions::Version, @user.versions.at(:first)
+      assert_kind_of VestalVersions::Version, @user.versions.at(:last)
+      assert_equal @times.keys.min, @user.versions.number_at(:first)
+      assert_equal @times.keys.max, @user.versions.number_at(:last)
+    end
+
+    should 'return nil when providing a version number for an invalid association extension method' do
+      assert_nil @user.versions.number_at(:INVALID)
+    end
+
+    should "provide a version number for any time after the model's creation" do
+      @times.each do |number, time|
+        assert_kind_of Fixnum, @user.versions.number_at(time + 30.minutes)
+        assert_equal number, @user.versions.number_at(time + 30.minutes)
+      end
+    end
+
+    should "provide a version number of 1 for a time before the model's creation" do
+      creation = @times.values.min
+      assert_equal 1, @user.versions.number_at(creation - 1.second)
+    end
+
+    should 'provide a version number for a given version object' do
+      @times.keys.each do |number|
+        version = @user.versions.at(number)
+        assert_kind_of VestalVersions::Version, version
+        assert_kind_of Fixnum, @user.versions.number_at(version)
+        assert_equal number, @user.versions.number_at(version)
+      end
     end
   end
 end
