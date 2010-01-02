8ad39f676294e9fcf50d9c764a84b91330f18cc4
laserlemon <steve.richert@gmail.com>
2010-01-02 14:21:17 -0500

    Added tests for the "versioned" feature.

---
+++ test/versioned_test.rb
@@ -0,0 +1,18 @@
+require File.join(File.dirname(__FILE__), 'test_helper')
+
+class VersionedTest < Test::Unit::TestCase
+  context 'ActiveRecord models' do
+    should 'respond to the "versioned?" method' do
+      assert ActiveRecord::Base.respond_to?(:versioned?)
+      assert User.respond_to?(:versioned?)
+    end
+
+    should 'return true for the "versioned?" method if the model is versioned' do
+      assert_equal true, User.versioned?
+    end
+
+    should 'return false for the "versioned?" method if the model is not versioned' do
+      assert_equal false, ActiveRecord::Base.versioned?
+    end
+  end
+end
