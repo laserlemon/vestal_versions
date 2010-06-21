075b6155c1cf8dbc6f19978b84c2069d634434e8
Lailson Bandeira <lailson@guava.com.br>
2010-06-20 22:02:18 -0300

    Changing require on test_helper to use the vestal_version of the project, not the one on the gem.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,7 @@ require 'test/unit'
 require 'active_record'
 require 'shoulda'
 require 'mocha'
-require 'vestal_versions'
+# require 'vestal_versions'
+require File.expand_path(File.join(File.dirname(__FILE__), '../lib/vestal_versions'))
 require 'schema'
 begin; require 'redgreen'; rescue LoadError; end
