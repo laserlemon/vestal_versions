1a9ec02c0c6aae600be481c3f0a37074dfb0fb19
Steve Richert <steve.richert@gmail.com>
2014-01-20 22:20:22 -0500

    Add bugfix for Rails 3.0 incompatibility with Ruby 2

--- lib/vestal_versions/versions.rb
+++ lib/vestal_versions/versions.rb
@@ -1,3 +1,21 @@
+# See: https://github.com/rails/rails/issues/11026
+if ActiveRecord::VERSION::MAJOR == 3 && ActiveRecord::VERSION::MINOR == 0 && RUBY_VERSION >= "2.0.0"
+  module ActiveRecord
+    module Associations
+      class AssociationProxy
+        def send(method, *args)
+          if proxy_respond_to?(method, true)
+            super
+          else
+            load_target
+            @target.send(method, *args)
+          end
+        end
+      end
+    end
+  end
+end
+
 module VestalVersions
   # An extension module for the +has_many+ association with versions.
   module Versions
